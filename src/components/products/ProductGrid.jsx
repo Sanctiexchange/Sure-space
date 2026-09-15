0ed6e7970276ac1500215728c462a34d3dc0978f
Sanctiexchange <sanctiexchange@gmail.com>
2026-09-15 11:18:58 +0100

    Approved Product rendering on home page

--- src/components/products/ProductGrid.jsx
+++ src/components/products/ProductGrid.jsx
@@ -1,20 +1,41 @@
-import products from "../../data/products"
+import { useEffect, useState } from "react"
+
+import { getMarketplaceProducts } from "../../utility/marketplaceProducts"
+
 import ProductCard from "./ProductCard"
+
 
 function ProductGrid() {
 
+  const [products, setProducts] = useState([])
+
+
+  useEffect(() => {
+
+    const marketplaceProducts = getMarketplaceProducts()
+
+    setProducts(marketplaceProducts)
+
+  }, [])
+
+
   return (
+
     <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 lg:grid-cols-4">
 
       {products.map((product) => (
+
         <ProductCard
           key={product.id}
           product={product}
         />
+
       ))}
 
     </div>
+
   )
 }
+
 
 export default ProductGrid
